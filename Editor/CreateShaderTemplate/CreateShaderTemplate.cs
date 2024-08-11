85be002e75085dd6fda7269881c94f11c542ab7a
WoBok <darwin.vinci.wkc@gmail.com>
2024-08-11 21:24:04 +0800

    DOTS Shader Temple

--- Editor/CreateShaderTemplate/CreateShaderTemplate.cs
+++ Editor/CreateShaderTemplate/CreateShaderTemplate.cs
@@ -5,12 +5,17 @@ public class CreateShaderTemplate : Editor
     [MenuItem("Assets/Create/Shader/URP Shader", false, -959)]
     static void CreateURPShaderFile()
     {
-        CreateShaderFile("URPShader", "New URP Shader.shader");
+        CreateShaderFile("URP_Shader", "New URP Shader.shader");
     }
     [MenuItem("Assets/Create/Shader/URP PBR Shader", false, -958)]
     static void CreateURPPBRShaderFile()
     {
         CreateShaderFile("URP_PBR_Shader", "New URP PBR Shader.shader");
+    }
+    [MenuItem("Assets/Create/Shader/URP DOTS Shader", false, -957)]
+    static void CreateURPDOTSShaderFile()
+    {
+        CreateShaderFile("URP_DOTS_Shader", "New URP DOTS Shader.shader");
     }
     static void CreateShaderFile(string filePath, string defaultNewFileName)
     {
--- Editor/CreateShaderTemplate/DoCreateShaderScritpAsset.cs
+++ Editor/CreateShaderTemplate/DoCreateShaderScritpAsset.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class DoCreateShaderScritpAsset : EndNameEditAction
 {
-    const string URPShader = @"
+    const string URP_Shader = @"
 Shader ""#PATH#"" {
     Properties {
         _BaseMap (""Albedo"", 2D) = ""white"" { }
@@ -160,6 +160,99 @@ Shader ""#PATH#"" {
     }
 }
 ";
+    const string URP_DOTS_Shader = @"
+Shader ""#PATH#"" {
+    Properties {
+        _BaseMap (""Albedo"", 2D) = ""white"" { }
+        _BaseColor (""Color"", Color) = (1, 1, 1, 1)
+    }
+
+    SubShader {
+        Tags { ""RenderPipeline"" = ""UniversalPipeline"" }
+
+        Pass {
+            HLSLPROGRAM
+
+            #pragma vertex Vertex
+            #pragma fragment Fragment
+            #pragma multi_compile_instancing
+
+            #include ""Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl""
+            #include_with_pragmas ""Packages/com.unity.render-pipelines.universal/ShaderLibrary/DOTS.hlsl""
+
+            struct Attributes {
+                float4 positionOS : POSITION;
+                float3 normalOS : NORMAL;
+                float2 texcoord : TEXCOORD0;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct Varyings {
+                float2 uv : TEXCOORD0;
+                float3 normalWS : TEXCOORD1;
+                float4 positionCS : SV_POSITION;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+            
+            TEXTURE2D(_BaseMap);
+            SAMPLER(sampler_BaseMap);
+
+            CBUFFER_START(UnityPerMaterial)
+            float4 _BaseMap_ST;
+            half4 _BaseColor;
+            CBUFFER_END
+
+            #ifdef UNITY_DOTS_INSTANCING_ENABLED
+
+                UNITY_DOTS_INSTANCING_START(MaterialPropertyMetadata)
+                UNITY_DOTS_INSTANCED_PROP(float4, _BaseMap_ST)
+                UNITY_DOTS_INSTANCED_PROP(float4, _BaseColor)
+                UNITY_DOTS_INSTANCING_END(MaterialPropertyMetadata)
+
+                static float4 unity_DOTS_Sampled_BaseMap_ST;
+                static half4 unity_DOTS_Sampled_BaseColor;
+
+                void SetupDOTSLitMaterialPropertyCaches() {
+                    unity_DOTS_Sampled_BaseMap_ST = UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float4, _BaseMap_ST);
+                    unity_DOTS_Sampled_BaseColor = UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float4, _BaseColor);
+                }
+
+                #undef UNITY_SETUP_DOTS_MATERIAL_PROPERTY_CACHES
+                #define UNITY_SETUP_DOTS_MATERIAL_PROPERTY_CACHES() SetupDOTSLitMaterialPropertyCaches()
+
+                #define _BaseMap_ST     unity_DOTS_Sampled_BaseMap_ST
+                #define _BaseColor      unity_DOTS_Sampled_BaseColor
+
+            #endif
+
+            Varyings Vertex(Attributes input) {
+
+                Varyings output;
+
+                UNITY_SETUP_INSTANCE_ID(input);
+                UNITY_TRANSFER_INSTANCE_ID(input, output);
+
+                output.positionCS = TransformObjectToHClip(input.positionOS.xyz);
+                output.normalWS = TransformObjectToWorldNormal(input.normalOS);
+                output.uv = TRANSFORM_TEX(input.texcoord, _BaseMap);
+
+                return output;
+            }
+
+            half4 Fragment(Varyings input) : SV_Target {
+
+                UNITY_SETUP_INSTANCE_ID(input);
+
+                half4 albedo = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.uv);
+                half4 diffuse = albedo * (dot(input.normalWS, normalize(_MainLightPosition.xyz)) * 0.5 + 0.5);
+
+                return diffuse * _BaseColor;
+            }
+            ENDHLSL
+        }
+    }
+}
+";
     public override void Action(int instanceId, string pathName, string resourceFile)
     {
         Object o = CreateScriptAssetFromTemplate(pathName, resourceFile);
@@ -173,14 +266,17 @@ Shader ""#PATH#"" {
         string templateText;
         switch (resourceFile)
         {
-            case "URPShader":
-                templateText = URPShader;
+            case "URP_Shader":
+                templateText = URP_Shader;
                 break;
             case "URP_PBR_Shader":
                 templateText = URP_PBR_Shader;
                 break;
+            case "URP_DOTS_Shader":
+                templateText = URP_DOTS_Shader;
+                break;
             default:
-                templateText = URPShader;
+                templateText = URP_Shader;
                 break;
         }
         templateText = templateText.Replace("#PATH#", $"URP Shader/{fileName}");
